3f28ed75a3ebbd49563f4dca42b7296c59754e78
jaekangchoi <jkchoi7091@knou.ac.kr>
2023-04-27 12:22:45 +0900

    Initial commit

---
+++ src/main/java/com/portfolio/board/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.portfolio.board.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
